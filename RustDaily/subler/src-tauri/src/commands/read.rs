3d5f6a7c2decd961282e1dc0d757e855e5fcd6be
Bikash9609 <bikashtiwari020@gmail.com>
2025-05-25 21:17:01 +0530

    chore: minor fixes

--- RustDaily/subler/src-tauri/src/commands/read.rs
+++ RustDaily/subler/src-tauri/src/commands/read.rs
@@ -1,23 +1,27 @@
 use dirs::home_dir;
+use serde::Serialize;
 use std::fs::{self, DirEntry};
 use std::path::PathBuf;
 
+#[dervice(Serialize)]
 struct File {
     path: String,
 }
 
 #[tauri::command]
-pub fn get_base() -> Result<Vec<DirEntry>, ()> {
+pub fn get_base() -> Result<Vec<File>, ()> {
     let mut path = home_dir().expect("Unable to recognize home directory!");
     path.push("Documents");
 
     match fs::read_dir(&path) {
         Ok(rd) => {
-            let mut paths: Vec<DirEntry> = Vec::new();
+            let mut paths: Vec<File> = Vec::new();
             for entry in rd {
                 match entry {
                     Ok(result_path) => {
-                        paths.push(result_path);
+                        paths.push(File {
+                            path: result_path.path().,
+                        });
                     }
                     Err(e) => {
                         eprintln!("{}", e);
